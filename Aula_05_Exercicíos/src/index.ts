9896f4536947244c7c478fdc5f13a74cad89733f
Andriel <andriel.h@aluno.ifsp.edu.br>
2024-03-01 19:37:12 -0300

    Aula_05

---
+++ Aula_05_Exercicíos/src/index.ts
@@ -0,0 +1,13 @@
+/*Exercício 01 - Escreva uma função em TypeScript que aceite um array de números como
+parâmetro e retorne o array ordenado em ordem crescente
+*/
+
+/*Exercício 02 - Escreva uma função em TypeScript que calcule a média ponderada de um
+conjunto de valores, dados seus valores e pesos correspondentes.
+*/
+
+/*Exercício 03 - Crie uma função em TypeScript que valide se um CPF é válido ou não. O CPF é
+composto por 11 dígitos numéricos. Considere a entrada como numérica. Para ser válido, ele deve
+seguir algumas regras específicas de formação e ter dígitos verificadores corretos, conforme a
+informações a seguir
+*/
