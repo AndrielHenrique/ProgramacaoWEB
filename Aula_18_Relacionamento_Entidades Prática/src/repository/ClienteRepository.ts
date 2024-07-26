d62bd80dcc18df17fb224c6c1c293bf27c80d25e
Andriel <andriel.h@aluno.ifsp.edu.br>
2024-07-26 20:29:23 -0300

    Falta terminar

--- Aula_18_Relacionamento_Entidades Prática/src/repository/ClienteRepository.ts
+++ Aula_18_Relacionamento_Entidades Prática/src/repository/ClienteRepository.ts
@@ -1,2 +1,39 @@
 import { executarComandoSQL } from "../database/mysql";
 import { Cliente } from "../model/Cliente";
+
+export class ClienteRepository {
+    
+    constructor(){
+        this.createTable();
+    }
+
+    private async createTable(){
+        const query = `
+        CREATE TABLE IF NOT EXISTS banco.cliente 
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(35) NOT NULL,
+        cpf VARCHAR(11) NOT NULL,
+        data_nascimento Date NOT NULL
+        ` 
+    try{
+        const resultado = await executarComandoSQL(query, []);
+        //console.log('Query executada com sucesso:', resultado);
+    } catch (err) {
+        console.error('Error');
+    }
+    }
+
+    async insereCliente(cliente: Cliente): Promise<Cliente>{
+        const query = "INSERT INTO banco.cliente (name, cpf, data_nascimento) VALUES (?, ?, ?) "
+        try{
+            const resultado = await executarComandoSQL(query, [cliente.name, cliente.cpf, cliente.data_nascimento]);
+            console.log('Cliente inserido com sucesso:', resultado.insertId);
+            cliente.id = resultado.insertId;
+            return cliente;
+        }catch (err) {
+            console.error('Erro ao adicionar dados do novo Cliente:', err);
+            throw err;
+        }
+        
+    }
+}
--- Aula_18_Relacionamento_Entidades Prática/src/service/ClienteService.ts
+++ Aula_18_Relacionamento_Entidades Prática/src/service/ClienteService.ts
@@ -2,13 +2,17 @@ import { Cliente } from "../model/Cliente";
 import { ClienteRepository } from "../repository/ClienteRepository";
 
 export class ClienteService {
-    clienteRepository = new clienteRepository();
+    clienteRepository = new ClienteRepository();
 
     async cadastrarCliente(dadosCliente: any): Promise<Cliente>{
         const { name, cpf, data_nascimento} = dadosCliente;
 
         if( typeof name !== 'string' || typeof cpf !== 'string' || typeof data_nascimento !== 'object' ){
             throw new Error("Informações incompletas ou incorretas");
+        }
+        const clienteResult = await this.clienteRepository.getClienteDados(undefined, cpf, undefined);
+        if(clienteResult.length == 0){
+            throw new Error("Dados do clienete informado inexistente.");
         }
         
         return this.clienteRepository.insereCliente(new Cliente(undefined, name, cpf, data_nascimento));
