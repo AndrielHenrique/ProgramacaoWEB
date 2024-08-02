93ac0052f0e93c2e071f73bdece7ed16f459b790
Andriel <andriel.h@aluno.ifsp.edu.br>
2024-08-02 20:26:08 -0300

    Aula 20 - Swagger

---
+++ Aula_20_Swagger/src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+
+const app = express();
+
+const PORT = 3040;
+
+app.use(express.json());
+
+
+
+app.listen(PORT, ()=> console.log("API online na porta: " + PORT));
